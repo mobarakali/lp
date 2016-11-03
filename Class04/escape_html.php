953d6e80d6cae91b1526a90f38540485c2b1346c
mobarakali <mobarak.ali@gmail.com>
2016-11-03 17:39:37 +0600

    finishing class on if

--- Class04/escape_html.php
+++ Class04/escape_html.php
@@ -11,19 +11,44 @@
 
 	$day = "Friday";
 
-	if($day == 'sunday')
+
+
+	if("Friday" == $day):// Dont write like this :  if($day =="Friday" )
+		echo " Bangladesh Hollyday!<br>";
+		echo " Yes it's  Hollyday!<br>";
+	endif;
+
+
+	if($day == 'sunday'):
 		echo "Amiracan Hollyday!<br>";
-	elseif($day == 'Friday')
+	elseif($day == 'Friday'):
 		echo " Bangladesh Hollyday!<br>";
 		echo " And We are from Bangladesh!<br>";
 
-	else
+	else:
 		echo " Amirican Workday!<br>";
+	endif;
 
-echo "<br> Outside If."
+echo "<br> Outside If.";
 
-
+if(true){
+	echo date("l");
+}elseif(false){
+	//
+}else{
+	//
+}
+$day = 'Dday';
 ?>
+		<!--// Escaping from html code-->
+
+		<?php if(date('l') == $day): ?>
+			<h1> Today is Thursday!</h1>
+			<?php else: ?>
+				<h1>It's not Thursday!</h1>
+				<?php endif ?>
+					<!--// elseif(false): // else: // endif;-->
+
 
 </body>
 
