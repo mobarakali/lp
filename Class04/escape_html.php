ba164241eda9a1261d65da0670a0dfc0d4db6fbe
mobarakali <mobarak.ali@gmail.com>
2016-11-03 17:11:19 +0600

    use of 'if' without brackets { }

--- Class04/escape_html.php
+++ Class04/escape_html.php
@@ -11,14 +11,17 @@
 
 	$day = "Friday";
 
-	if($day == 'sunday'){
-		echo "Amiracan Hollyday!";
-	}elseif($day == 'Friday'){
-		echo " Bangladesh Hollyday!";
-	}else{
-		echo " Amirican Workday!";
-	}
+	if($day == 'sunday')
+		echo "Amiracan Hollyday!<br>";
+	elseif($day == 'Friday')
+		echo " Bangladesh Hollyday!<br>";
+		echo " And We are from Bangladesh!<br>";
+
+	else
+		echo " Amirican Workday!<br>";
+
 echo "<br> Outside If."
+
 
 ?>
 
