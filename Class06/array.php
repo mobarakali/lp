1a24ae7644abaeb6471e2b06fb5b54978868f881
mobarakali <mobarak.ali@gmail.com>
2016-11-17 16:37:20 +0600

    php array class 06

---
+++ Class06/array.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Array</title>
+</head>
+
+<body>
+	<h1>Learning Array</h1>
+
+	<?php
+		// divisions in bagladesh
+		$div1= "Dhaka";
+		$div1_dis1= " Dhaka";
+		$div1_dis2= " Tangaile";
+		$div2= "Chittagong";
+		$div3= "Rajshsahi";
+		$div4= "Khulna";
+
+		//Old way;
+		$div = array("Dhaka","Chattagong", "khulna", "Rajshahi");
+		echo $div[0];
+		var_dump($div);
+	/*
+	([Dhaka][Chattagong][khulna][Rajshahi])
+		0		1			2		3
+
+	*/?>
+
+
+</body>
+
+</html>
---
+++ homework/nov_17.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<style>
+		td {
+			border: 2px solid black;
+			text-align: center;
+			padding: 5px;
+		}
+
+	</style>
+
+</head>
+
+<body>
+	<h2>Novemder 2016</h2>
+	<table>
+		<tr>
+			<td>sunday</td>
+			<td>monday</td>
+			<td>tuesday</td>
+			<td>wednesday</td>
+			<td>thursday</td>
+			<td>friday</td>
+			<td>saturday</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+
+		</tr>
+		<tr>
+			<td>6</td>
+			<td>7</td>
+			<td>8</td>
+			<td>9</td>
+			<td>10</td>
+			<td>11</td>
+			<td>12</td>
+
+		</tr>
+		<tr>
+			<td>13</td>
+			<td>14</td>
+			<td>15</td>
+			<td>16</td>
+			<td>17</td>
+			<td>18</td>
+			<td>19</td>
+
+		</tr>
+		<tr>
+			<td>20</td>
+			<td>21</td>
+			<td>22</td>
+			<td>23</td>
+			<td>24</td>
+			<td>25</td>
+			<td>26</td>
+
+		</tr>
+		<tr>
+			<td>27</td>
+			<td>28</td>
+			<td>29</td>
+			<td>30</td>
+			<td></td>
+			<td></td>
+			<td></td>
+
+
+		</tr>
+	</table>
+
+</body>
+
+</html>
