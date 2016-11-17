a514a71240b878232377fa2a756d6f96ce2804fd
mobarakali <mobarak.ali@gmail.com>
2016-11-17 16:56:43 +0600

    array01 ends

--- Class06/array.php
+++ Class06/array.php
@@ -19,9 +19,32 @@
 		$div4= "Khulna";
 
 		//Old way;
-		$div = array("Dhaka","Chattagong", "khulna", "Rajshahi");
-		echo $div[0];
+		$div = array("Dhaka","Chattagong", "khulna", "Rajshahi","Sylhet");
+
+		//$div[5] = "Ranjpur";
+		$div[] = "Ranjpur";
+		//$div[] = "Barishal";
+		$div[] = "Barishal";
+
+
+	// 5.4 Version
+
+	$bivag = ['ঢাকা', 'চট্টগ্রাম', 'খুলনা', 'রাজশাহী'];
+
+	// var $বিভাগ; //
+	echo '<pre>';
+	print_r($bivag);
+	echo '</pre>';
+		var_dump($bivag);
+
+
+		// echo $div[0];
+	echo '<pre>';
+	print_r($div);
+	echo '</pre>';
+
 		var_dump($div);
+
 	/*
 	([Dhaka][Chattagong][khulna][Rajshahi])
 		0		1			2		3
