b29e50bf48619a4b4aa94f335c81b5e4d4f0cba0
mobarakali <mobarak.ali@gmail.com>
2016-12-06 11:22:15 +0600

    batch2 class 2

---
+++ batch02/Class02/variable.php
@@ -0,0 +1,42 @@
+<?php
+// 1.0 Naming php variable
+
+// a-z,
+// A-Z,
+
+// can be used at beginning with any numaric or a-Z
+// _
+// $
+
+// other character
+//0-9
+
+
+// Case Sencetive
+// $a is not $A
+
+$azAZ; // variable dec. // (initialization)
+
+$az = 2; // Dec. and assinging a vlue
+echo $az;
+
+$azAZ= 44; // assinging a vlue
+
+print_r($azAZ);
+
+$num2 = 2;
+
+print $num2; // output 2
+
+$num2 = 555; // re assing new value
+
+/*
+print $num2;
+
+echo 'I am echo <br>';
+
+print " $num2, $num2 ";
+echo '<br>';
+echo $num2, $num2; */
+
+?>
