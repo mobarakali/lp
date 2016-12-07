5406a4a7baf6f18c88e4374d374806be27d4e6c8
mobarakali <mobarak.ali@gmail.com>
2016-12-07 10:54:26 +0600

    finishing class02

---
+++ batch02/Class02/data-type.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Datatype</title>
+</head>
+<body>
+	<h1>Data Type</h1>
+	<h2> Boolean</h2>
+	<pre>
+	<?php
+	$Iam_a_doctor= false;
+	echo $Iam_a_doctor;
+
+	var_dump($Iam_a_doctor);
+
+	?>
+
+	</pre>
+
+	<h2> Integer</h2>
+	<pre>
+	<?php
+	$num= 2;
+	// $num = false; // you can change the value type any time
+	echo $num;
+
+	var_dump($num);
+
+	?>
+
+	</pre>
+
+	<h2> Float</h2>
+	<pre>
+	<?php
+	$faction = 2.22222;
+
+	echo $faction;
+
+	var_dump($faction);
+
+	?>
+
+	</pre>
+
+	<h2> String </h2>
+	<pre>
+	<?php
+	$faction = "some text" ;
+
+	echo $faction;
+
+	var_dump($faction);
+
+	?>
+	</pre>
+
+
+<!--আমি হব সকাল বেলার পাখী
+			সবার আগে কুসম বাগে উঠব আমি ডাকি-->
+</body>
+</html>
--- batch02/Class02/variable.php
+++ batch02/Class02/variable.php
@@ -11,7 +11,7 @@
 // other character
 //0-9
 
-
+// *
 // Case Sencetive
 // $a is not $A
 
@@ -19,24 +19,32 @@ $azAZ; // variable dec. // (initialization)
 
 $az = 2; // Dec. and assinging a vlue
 echo $az;
+print '<br>';
 
 $azAZ= 44; // assinging a vlue
 
 print_r($azAZ);
+print '<br>';
 
+
+echo $az,':',$azAZ;
+//*/
+print '<br>';
 $num2 = 2;
 
-print $num2; // output 2
+// will make a Parse Error (Syntax Error)
+// and halt!
+print $num2, '<br>';
 
 $num2 = 555; // re assing new value
 
-/*
 print $num2;
+
 
 echo 'I am echo <br>';
 
 print " $num2, $num2 ";
 echo '<br>';
-echo $num2, $num2; */
+echo $num2, $num2;
 
 ?>
